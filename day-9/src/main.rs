4c508f556d05a5bb548680ebf06c48c14595ea6e
Aurélien Montmejat <aurelien.montmejat@gmail.com>
2023-12-10 12:23:59 +0100

    day 9

--- day-9/src/main.rs
+++ day-9/src/main.rs
@@ -4,5 +4,4 @@ mod part_two;
 fn main() {
     part_one::part_one();
     part_two::part_two();
-    part_two::part_two_slow();
 }
---
+++ day-9/src/part_one.rs
@@ -0,0 +1,42 @@
+use std::fs;
+
+pub fn part_one() {
+    let input = fs::read_to_string("input").expect("Something went wrong reading the file");
+
+    let sequences = input
+        .lines()
+        .map(|x| {
+            x.split(" ")
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>()
+        })
+        .collect::<Vec<Vec<i32>>>();
+
+    let mut predictions_sum = 0;
+
+    for sequence in sequences {
+        let mut differences = vec![sequence];
+
+        loop {
+            let mut curr_diffs = Vec::new();
+
+            for j in 0..differences.last().unwrap().len() - 1 {
+                curr_diffs.push(differences.last().unwrap()[j + 1] - differences.last().unwrap()[j])
+            }
+
+            if curr_diffs.iter().sum::<i32>() == 0 {
+                break;
+            }
+            differences.push(curr_diffs);
+        }
+
+        let mut prediction = 0;
+        for diffs in differences.iter().rev() {
+            prediction = diffs.last().unwrap() + prediction;
+        }
+
+        predictions_sum += prediction;
+    }
+
+    println!("Sum of predictions: {}", predictions_sum);
+}
